e01b20a772ef956744daa13b715aa604d2fbc2a7
Jingyuan Lu <jilu0993@uni.sydney.edu.au>
2025-05-26 22:24:02 +1000

    test

--- iam/main.tf
+++ iam/main.tf
@@ -1,22 +1,7 @@
-resource "aws_iam_role" "ec2_role" {
-  name = "EC2InstanceRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-
 resource "aws_iam_role_policy_attachment" "ec2_full_access" {
-  role       = aws_iam_role.ec2_role.name
+  role       = "EC2InstanceRole"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "EC2InstanceProfile"
-  role = aws_iam_role.ec2_role.name
-}
+
+
