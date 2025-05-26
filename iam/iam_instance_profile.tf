cd7c5d1da8872e64c1985fd834aa09ddb16c68fb
Jingyuan Lu <jilu0993@uni.sydney.edu.au>
2025-05-26 21:40:19 +1000

    test

--- iam/iam_instance_profile.tf
+++ iam/iam_instance_profile.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "EC2InstanceProfile"
-  role = aws_iam_role.ec2_role.name
-}
+# resource "aws_iam_instance_profile" "ec2_profile" {
+#   name = "EC2InstanceProfile"
+#   role = aws_iam_role.ec2_role.name
+# }
