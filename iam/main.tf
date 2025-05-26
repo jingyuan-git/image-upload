a0cb5d5ed63b2f32b0bc588b5348e0644ab74f97
Jingyuan Lu <jilu0993@uni.sydney.edu.au>
2025-05-26 22:46:07 +1000

    test

--- iam/main.tf
+++ iam/main.tf
@@ -1,18 +1,3 @@
-resource "aws_iam_role_policy" "allow_attach_policy" {
-  name = "AllowAttachPolicyToEC2InstanceRole"
-  role = "EC2InstanceRole"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["iam:AttachRolePolicy"]
-        Resource = "arn:aws:iam::649490358042:role/EC2InstanceRole"
-      }
-    ]
-  })
-}
 
 resource "aws_iam_role_policy_attachment" "ec2_full_access" {
   role       = "EC2InstanceRole"
