f1d5940cda3573750104104b28133e284176466e
Jingyuan Lu <jilu0993@uni.sydney.edu.au>
2025-05-26 20:48:45 +1000

    test

--- network/outputs.tf
+++ network/outputs.tf
@@ -8,12 +8,12 @@ output "private_subnet_rds_id" {
   value       = aws_subnet.private_subnet_rds.id
 }
 
-output "private_subnet_rds_name" {
-  description = "The name of the DB subnet group"
-  value       = aws_subnet.private_subnet_rds.name
-}
-
 output "security_group_id" {
   description = "The ID of the web security group"
   value       = aws_security_group.web_sg.id
+}
+
+output "db_subnet_group_name" {
+  description = "The name of the DB subnet group"
+  value       = aws_db_subnet_group.rds_subnet_group.name
 }
