0b027dd30db8e2fdb015ac79464649a1757dad5a
Jingyuan Lu <jilu0993@uni.sydney.edu.au>
2025-05-26 21:50:06 +1000

    test

--- iam/main.tf
+++ iam/main.tf
@@ -10,17 +10,13 @@
 #   })
 # }
 
-data "aws_iam_role" "existing_ec2_role" {
-  name = "EC2InstanceRole"
-}
-
 
 resource "aws_iam_role_policy_attachment" "ec2_full_access" {
-  role       = data.aws_iam_role.existing_ec2_role.name
+  role       = "EC2InstanceRole"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "EC2InstanceProfile"
-  role = data.aws_iam_role.existing_ec2_role.name
+  role = "EC2InstanceRole"
 }
