7c7db6e78ee2c0ccfb3d8358b845ec73e3749703
Jingyuan Lu <jilu0993@uni.sydney.edu.au>
2025-05-29 10:39:23 +1000

    test

--- lambda/annotation/lambda.tf
+++ lambda/annotation/lambda.tf
@@ -28,23 +28,42 @@ variable "db_password" {
   type        = string
 }
 
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}"
-  output_path = "${path.module}/lambda.zip"
-}
-
 data "aws_iam_role" "existing_lambda_role" {
   name = "LabRole" # 替换为已有角色的名称
 }
+
+# data "archive_file" "lambda_zip" {
+#   type        = "zip"
+#   source_dir  = "${path.module}"
+#   output_path = "${path.module}/lambda.zip"
+# }
+# resource "aws_lambda_function" "annotation" {
+#   function_name    = "annotation-function"
+#   handler          = "main.lambda_handler"
+#   runtime          = "python3.12"
+#   role             = data.aws_iam_role.existing_lambda_role.arn
+#   filename         = data.archive_file.lambda_zip.output_path
+#   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+#   timeout          = 30
+
+#   environment {
+#     variables = {
+#       GOOGLE_API_KEY = var.google_api_key
+#       DB_HOST       = var.db_host
+#       DB_USER       = var.db_user
+#       DB_PASSWORD   = var.db_password
+#       DB_NAME       = "image_caption_db"
+#     }
+#   }
+# }
 
 resource "aws_lambda_function" "annotation" {
   function_name    = "annotation-function"
   handler          = "main.lambda_handler"
   runtime          = "python3.12"
   role             = data.aws_iam_role.existing_lambda_role.arn
-  filename         = data.archive_file.lambda_zip.output_path
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  filename         = "${path.module}/lambda_function.zip"  # 直接指向已有的 zip 文件
+  source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")  # 使用文件的哈希值
   timeout          = 30
 
   environment {
