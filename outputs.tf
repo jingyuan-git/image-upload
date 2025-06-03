d6ab8868c87999c0ad47d78c29e9f2d44504b4d9
Jingyuan Lu <jilu0993@uni.sydney.edu.au>
2025-06-03 15:18:32 +1000

    test

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 
-output "instance_public_ip" {
-  value = module.ec2.instance_public_ip
-}
+# output "instance_public_ip" {
+#   value = module.ec2.instance_public_ip
+# }
 
 output "rds_endpoint" {
   description = "The endpoint of the RDS instance"
