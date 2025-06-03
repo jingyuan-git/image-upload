65aa085e497cf7c3fce4e878ee4944dcfca98d4c
Jingyuan Lu <jilu0993@uni.sydney.edu.au>
2025-06-03 16:36:10 +1000

    test

--- network/main.tf
+++ network/main.tf
@@ -244,6 +244,11 @@ resource "aws_route_table" "public_route_table_az2" {
   }
 }
 
+resource "aws_route_table_association" "public_subnet_association_az2" {
+  subnet_id      = aws_subnet.public_subnet_az2.id
+  route_table_id = aws_route_table.public_route_table_az2.id
+}
+
 resource "aws_route_table_association" "private_subnet_association" {
   subnet_id      = aws_subnet.private_subnet_web.id
   route_table_id = aws_route_table.private_route_table.id
